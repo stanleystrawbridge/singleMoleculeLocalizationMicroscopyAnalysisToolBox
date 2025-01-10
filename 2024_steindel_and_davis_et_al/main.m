c4a5f0f10b68ed9b82f198833c239c6e9ec87bd6
Stanley Strawbridge <stanley.strawbridge@gmail.com>
2025-01-10 13:35:34 +0000

    update

--- 2024_steindel_and_davis_et_al/main.m
+++ 2024_steindel_and_davis_et_al/main.m
@@ -5,7 +5,7 @@ addpath('src')
 
 data_sets = {'small_data_set_out','large_data_set_out'};
 
-for i = 1:numel(data_sets)
+for i = 2:numel(data_sets)
    
     output_folder = fullfile('output',data_sets{i});
     mkSaveFolder(output_folder)   
